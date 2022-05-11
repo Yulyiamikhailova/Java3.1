919ae1ebb93183d0e5174f7d908696f7e5331189
“Yulia” <Yulyiamikhailova@gmail.com>
2022-05-11 15:02:10 +0300

    init commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int price = 10000;
+        int bonus = 20;
+        int miles = price / bonus;
+        return miles;
+    }
+
+}
